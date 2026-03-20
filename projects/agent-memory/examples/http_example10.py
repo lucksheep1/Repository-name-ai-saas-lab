aa2d5c08a02220612c876a6c35fd6d7f6d598c3f
AI SaaS Lab <ai-saas-lab@example.com>
2026-03-20 20:18:54 +0800

    feat: add http_example10

---
+++ projects/agent-memory/examples/http_example10.py
@@ -0,0 +1,14 @@
+"""
+Memory http_example10
+http_example10
+"""
+import http.server
+import socketserver
+
+
+def demo():
+    print(http.server.SimpleHTTPRequestHandler)
+
+
+if __name__ == "__main__":
+    demo()
